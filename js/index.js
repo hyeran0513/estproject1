b6e3bd9ecf5d589d9deaba32f2d7f496abcf16f0
hyeran0513 <hr_0513@naver.com>
2024-12-09 12:57:10 +0900

    241209 김혜란 프로젝트 생성 및 폴더 구조 설정

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// header.html 불러오기
+fetch("components/common/header.html")
+  .then((response) => response.text())
+  .then((data) => (document.getElementById("header").innerHTML = data));
+
+// footer.html 불러오기
+fetch("components/common/footer.html")
+  .then((response) => response.text())
+  .then((data) => (document.getElementById("footer").innerHTML = data));
+
+// banner.html 불러오기
+fetch("components/common/banner.html")
+  .then((response) => response.text())
+  .then((data) => (document.getElementById("banner").innerHTML = data));
+
+// about.html 불러오기
+fetch("components/about.html")
+  .then((response) => response.text())
+  .then((data) => (document.getElementById("about").innerHTML = data));
+
+// hrkim.html 불러오기
+fetch("components/hrkim.html")
+  .then((response) => response.text())
+  .then((data) => (document.getElementById("hrkim").innerHTML = data));
