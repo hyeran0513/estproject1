9bdf91b652858e82c8729f4822669cf0145fc20e
hyeran0513 <hr_0513@naver.com>
2024-12-09 13:09:30 +0900

    241209 김혜란 baseurl 추가

--- js/index.js
+++ js/index.js
@@ -1,24 +1,33 @@
-// header.html 불러오기
-fetch("components/common/header.html")
-  .then((response) => response.text())
-  .then((data) => (document.getElementById("header").innerHTML = data));
+document.addEventListener('DOMContentLoaded', () => {
+  // baseurl 처리
+  if (window.location.hostname === 'hyeran0513.github.io') {
+    var base = document.createElement('base');
+    base.href = '/estproject1/';
+    document.head.appendChild(base);
+  }
 
-// footer.html 불러오기
-fetch("components/common/footer.html")
-  .then((response) => response.text())
-  .then((data) => (document.getElementById("footer").innerHTML = data));
+  // header.html 불러오기
+  fetch("components/common/header.html")
+    .then((response) => response.text())
+    .then((data) => (document.getElementById("header").innerHTML = data));
 
-// banner.html 불러오기
-fetch("components/common/banner.html")
-  .then((response) => response.text())
-  .then((data) => (document.getElementById("banner").innerHTML = data));
+  // footer.html 불러오기
+  fetch("components/common/footer.html")
+    .then((response) => response.text())
+    .then((data) => (document.getElementById("footer").innerHTML = data));
 
-// about.html 불러오기
-fetch("components/about.html")
-  .then((response) => response.text())
-  .then((data) => (document.getElementById("about").innerHTML = data));
+  // banner.html 불러오기
+  fetch("components/common/banner.html")
+    .then((response) => response.text())
+    .then((data) => (document.getElementById("banner").innerHTML = data));
 
-// hrkim.html 불러오기
-fetch("components/hrkim.html")
-  .then((response) => response.text())
-  .then((data) => (document.getElementById("hrkim").innerHTML = data));
+  // about.html 불러오기
+  fetch("components/about.html")
+    .then((response) => response.text())
+    .then((data) => (document.getElementById("about").innerHTML = data));
+
+  // hrkim.html 불러오기
+  fetch("components/hrkim.html")
+    .then((response) => response.text())
+    .then((data) => (document.getElementById("hrkim").innerHTML = data));
+});
