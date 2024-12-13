4c0e0dfc9b882adc5b17ec544185b50fccd34e7b
hyeran0513 <hr_0513@naver.com>
2024-12-13 21:54:58 +0900

    241213 김혜란 배너, 직원 소개, 푸터, 헤더, 문의 PC 작업 완료

--- js/index.js
+++ js/index.js
@@ -16,6 +16,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // 카드 애니메이션 관찰
   observeCards();
+
+  // 지도 이벤트 핸들러
+  handleMap();
 });
 
 // baseurl 처리
@@ -32,7 +35,7 @@ const handleScrollEvent = () => {
   const header = document.querySelector(".header");
 
   window.addEventListener("scroll", () => {
-    if (window.scrollY >= 500) {
+    if (window.scrollY >= 400) {
       header.classList.add("active");
     } else {
       header.classList.remove("active");
@@ -63,7 +66,7 @@ const initializeSwiper = () => {
   });
 };
 
-// 슬라이드 변경 이벤트 핸들러 추가
+// 슬라이드 변경 이벤트 핸들러
 const handleSlideChange = (swiper) => {
   swiper.on("slideChange", () => {
     const currentIndex = swiper.realIndex + 1;
@@ -108,4 +111,49 @@ const observeCards = () => {
   );
 
   cards.forEach((card) => observer.observe(card)); // 각 카드 요소를 관찰
+};
+
+// 지도 이벤트 핸들러
+const handleMap = () => {
+  // 지도 생성
+  const container = document.getElementById("map");
+  const options = {
+    center: new kakao.maps.LatLng(37.5665, 126.978), // 초기값: 서울 중심
+    level: 3,
+  };
+
+  const map = new kakao.maps.Map(container, options);
+
+  // 주소-좌표 변환 객체 생성
+  const geocoder = new kakao.maps.services.Geocoder();
+
+  // 입력된 주소
+  const address = "서울시 서초구 반포대로3 이스트빌딩(우)06711";
+
+  // 주소로 좌표를 검색
+  geocoder.addressSearch(address, function (result, status) {
+    if (status === kakao.maps.services.Status.OK) {
+      const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+      // 결과 좌표를 지도 중심으로 설정
+      map.setCenter(coords);
+
+      // 마커 생성
+      const marker = new kakao.maps.Marker({
+        map: map,
+        position: coords,
+      });
+
+      const infoContent = `
+        <a href="https://map.kakao.com/link/to/이스트빌딩,${result[0].y},${result[0].x}" target="_blank" class="link">
+          <i class="fa-solid fa-map-location-dot"></i>길찾기
+        </a>
+      `;
+
+      const findRouteBtn = document.querySelector(".btn-find-route");
+      findRouteBtn.innerHTML = infoContent;
+    } else {
+      console.error("주소 검색에 실패했습니다:", status);
+    }
+  });
 };
