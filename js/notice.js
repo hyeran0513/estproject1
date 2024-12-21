f79ea19dd6a9c111c1c436f011b8ca554e9507dc
hyeran0513 <hr_0513@naver.com>
2024-12-21 23:30:02 +0900

    241221 김혜란 서브 페이지 추가 (포트폴리오, 인터뷰, 뉴스, 공지사항, 문의)

---
+++ js/notice.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // 탭 기능
+  document.querySelectorAll(".tab__button").forEach((button) => {
+    button.addEventListener("click", () => {
+      const targetTab = button.getAttribute("data-tab");
+
+      document
+        .querySelectorAll(".tab__button")
+        .forEach((btn) => btn.classList.remove("active"));
+      document
+        .querySelectorAll(".notice__list")
+        .forEach((panel) => panel.classList.remove("active"));
+
+      button.classList.add("active");
+      document.getElementById(targetTab).classList.add("active");
+    });
+  });
+});
