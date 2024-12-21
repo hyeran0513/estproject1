d55c959513322cf6afc183cdda967e5e9017f2a1
hyeran0513 <hr_0513@naver.com>
2024-12-22 03:28:17 +0900

    241222 김혜란 서브페이지 생성 (회사소개, 직원소개, 뉴스, 공지사항, 포트폴리오)

--- js/index.js
+++ js/index.js
@@ -137,6 +137,51 @@ const handleZoomPortfolio = () => {
   });
 };
 
+// 카카오맵 핸들러
+const handleMap = () => {
+  const container = document.getElementById("map");
+  const options = {
+    center: new kakao.maps.LatLng(37.5665, 126.978), // 초기값: 서울 중심
+    level: 3,
+  };
+
+  const map = new kakao.maps.Map(container, options);
+
+  // 주소-좌표 변환 객체 생성
+  const geocoder = new kakao.maps.services.Geocoder();
+
+  // 입력된 주소
+  const address = "서울시 서초구 반포대로3 이스트빌딩(우)06711";
+
+  // 주소로 좌표를 검색
+  geocoder.addressSearch(address, (result, status) => {
+    if (status === kakao.maps.services.Status.OK) {
+      const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+      // 결과 좌표를 지도 중심으로 설정
+      map.setCenter(coords);
+
+      // 마커 생성
+      const marker = new kakao.maps.Marker({
+        map: map,
+        position: coords,
+      });
+
+      // 길찾기 버튼
+      const infoContent = `
+        <a href="https://map.kakao.com/link/to/이스트빌딩,${result[0].y},${result[0].x}" target="_blank" class="link">
+          <i class="fa-solid fa-map-location-dot"></i>길찾기
+        </a>
+      `;
+
+      const findRouteBtn = document.querySelector(".btn-find-route");
+      findRouteBtn.innerHTML = infoContent;
+    } else {
+      console.error("주소 검색에 실패했습니다:", status);
+    }
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   // swiper 객체 초기화 및 이벤트 핸들러 추가
   const swiper = initializeSwiper();
@@ -149,9 +194,8 @@ document.addEventListener("DOMContentLoaded", () => {
     ".company__info",          // 회사 소개
     ".company__thumbnail",
     ".company__card-item",
-    ".vision__text-box",       // 비전
-    ".vision__image",
-    ".interview__card-item"    // 인터뷰
+    ".interview__card-item",    // 인터뷰
+    ".influence__description"   // 영향
   ];
   elementsToObserve.forEach(selector => observeElements(selector));
 
@@ -161,4 +205,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // 로딩 애니메이션
   loading();
+
+  // 카카오맵 핸들러
+  handleMap();
 });
