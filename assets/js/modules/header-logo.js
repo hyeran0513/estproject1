154964ee1b017d1cd3231fa54c5735ef3e5ceb32
hyeran0513 <hr_0513@naver.com>
2025-01-21 17:21:38 +0900

    250121 김혜란 반응형 mixin 함수 적용

--- assets/js/modules/header-logo.js
+++ assets/js/modules/header-logo.js
@@ -5,14 +5,25 @@ export const updateHeaderLogo = () => {
 
   if (headerLogoLink && headerLogoImg) {
     const theme = document.documentElement.getAttribute("data-theme");
-
     const path = window.location.pathname;
-    const baseUrl = window.baseUrl;
+    const baseUrl = window.baseUrl.endsWith("/")
+      ? window.baseUrl
+      : `${window.baseUrl}/`;
+
+    // 경로 끝의 슬래시 제거
+    const normalizePath = (url) => url.replace(/\/+$/, "");
+    const isHomePage =
+      normalizePath(path) === normalizePath(baseUrl) ||
+      path === `${baseUrl}index.html`;
+
+    console.log("Theme:", theme);
+    console.log("Path:", path);
+    console.log("Base URL:", baseUrl);
+    console.log("Is Home Page:", isHomePage);
 
     const getLogoSrc = () => {
       const isDarkTheme = theme === "dark";
       const isLightTheme = theme === "light";
-      const isHomePage = path === baseUrl || path === `${baseUrl}index.html`;
 
       if (isDarkTheme) {
         return `${baseUrl}assets/images/icon/common/icon_logo_white.svg`;
@@ -29,7 +40,7 @@ export const updateHeaderLogo = () => {
       return `${baseUrl}assets/images/icon/common/icon_logo_white.svg`;
     };
 
-    headerLogoImg.src = getLogoSrc(true);
+    headerLogoImg.src = getLogoSrc();
     headerLogoLink.href = baseUrl;
   }
 };
