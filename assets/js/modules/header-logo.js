2ba7701efd468e0a962f0ed0b4bcd1ad9062e9bd
hyeran0513 <hr_0513@naver.com>
2025-01-21 17:06:37 +0900

    250121 김혜란 헤더 조건문 baseurl로 처리

--- assets/js/modules/header-logo.js
+++ assets/js/modules/header-logo.js
@@ -6,17 +6,13 @@ export const updateHeaderLogo = () => {
   if (headerLogoLink && headerLogoImg) {
     const theme = document.documentElement.getAttribute("data-theme");
 
-    console.log(theme + "???????");
     const path = window.location.pathname;
     const baseUrl = window.baseUrl;
 
     const getLogoSrc = () => {
       const isDarkTheme = theme === "dark";
       const isLightTheme = theme === "light";
-      const isHomePage = path === "/" || path === "/index.html";
-
-      console.log(isHomePage);
-      console.log(header.classList.contains("--active"));
+      const isHomePage = path === baseUrl || path === `${baseUrl}index.html`;
 
       if (isDarkTheme) {
         return `${baseUrl}assets/images/icon/common/icon_logo_white.svg`;
